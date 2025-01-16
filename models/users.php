e72590f06bb3af68aba5f9305ee9fb240dff2c21
majmehdi <majmehdi20@gmail.com>
2025-01-16 11:23:12 +0100

    I'm done with the create method on the user model

--- models/users.php
+++ models/users.php
@@ -7,61 +7,39 @@ abstract class users implements userInterface, interfaceCRUD  {
     
     protected $db;
     protected $id;
-    protected $user_serial;
+    protected $username;
     protected $email;
     protected $role;
 
 
-
+// this is the users constructor 
     public function __construct($db, $userData = null) {
         $this->db = $db;
         if (userData){
             $this->id = userData['id'];
-            $this->user_serial = userData['user_serial'];
+            $this->	username = userData['username'];
             $this->email = userData['email'];
 
         }
     }
 
-    public function register() {
-        $query = "INSERT INTO " . $this->table_name . " 
-                  (user_serial, first_name, last_name, age, email, password, role,  status) 
-                  VALUES (:user_serial, :first_name, :last_name, :age, :email, :password, :role, :status)";
+    // I'm going to implement the CRUD here using the crud interface that I've created before 
 
-        $stmt = $this->conn->prepare($query);
+    // create
+    
+    public function create($data){
 
-        $this->user_serial = $this->sanitizeInput($this->user_serial);
-        $this->first_name = $this->sanitizeInput($this->first_name);
-        $this->last_name = $this->sanitizeInput($this->last_name);
-        $this->age = $this->sanitizeInput($this->age);
-        $this->email = $this->sanitizeInput($this->email);
-        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
-        $this->role = $this->sanitizeInput($this->role);
-        $this->status = ($this->role === 'student') ? 'accept' : 'in progress';
+        $query = "INSERT INTO users (username, email, password, role ) VALUES (:username , :email, :password, :role)";
 
-        $stmt->bindParam(":user_serial", $this->user_serial);
-        $stmt->bindParam(":first_name", $this->first_name);
-        $stmt->bindParam(":last_name", $this->last_name);
-        $stmt->bindParam(":age", $this->age);
-        $stmt->bindParam(":email", $this->email);
-        $stmt->bindParam(":password", $this->password);
-        $stmt->bindParam(":role", $this->role);
-        $stmt->bindParam(":status", $this->status);
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam('username', data['username']);
+        $stmt->bindParam('email', data['email']);
+        $stmt->bindParam('password', password_hash(data['password'], PASSWORD_BCRYPT)); 
+        $stmt->bindParam('role',data['role']);
 
-        try {
-            if($stmt->execute()) {
-                return true;
-            }
-            $this->error_message = "Error while executing query.";
-            error_log("Sign UP ERROR: " . $this->error_message);
-            return false;
-        } catch (PDOException $e) {
-            $this->error_message = "PDO ERROR : " . $e->getMessage();
-            error_log("Sign UP ERROR : " . $this->error_message);
-            return false;
-        }
+        return $stmt->execute();
     }
-
+    
 
 
 
