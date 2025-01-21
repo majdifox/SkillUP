4e1f59aeef010ec9728497ff4d1fbaf03f0a9547
majmehdi <majmehdi20@gmail.com>
2025-01-21 22:23:25 +0100

    I've added the CRUD for the Category class

--- classes/category.php
+++ classes/category.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'interfaceCRUD.php';
+require_once 'CrudInterface.php';
 
 class Category implements CrudInterface {
     private $db;
@@ -9,33 +9,50 @@ class Category implements CrudInterface {
     }
 
     public function create($data) {
-        $query = "INSERT INTO category (category_name) VALUES (:category_name)";
+        $query = "INSERT INTO categories (name) VALUES (:name)";
         $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':category_name', $data['category_name']);
+        $stmt->bindParam(':name', $data['name']);
         return $stmt->execute();
     }
 
-    public function read($category_id) {
-        $query = "SELECT * FROM category WHERE category_id = :category_id";
+    public function read($id) {
+        $query = "SELECT * FROM categories WHERE category_id = :id";
         $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':category_id', $category_id);
+        $stmt->bindParam(':id', $id);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function update($category_id, $data) {
-        $query = "UPDATE category SET category_name = :category_name WHERE category_id = :category_id";
+    public function update($id, $data) {
+        $query = "UPDATE categories SET name = :name WHERE category_id = :id";
         $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':category_name', $data['category_name']);
-        $stmt->bindParam(':category_id', $category_id);
+        $stmt->bindParam(':name', $data['name']);
+        $stmt->bindParam(':id', $id);
         return $stmt->execute();
     }
 
-    public function delete($category_id) {
-        $query = "DELETE FROM category WHERE category_id = :category_id";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':category_id', $category_id);
-        return $stmt->execute();
+    public function delete($id) {
+        try {
+            $this->db->beginTransaction();
+            
+            // First update all courses with this category to NULL
+            $query = "UPDATE courses SET category_id = NULL WHERE category_id = :id";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            
+            // Then delete the category
+            $query = "DELETE FROM categories WHERE category_id = :id";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            
+            $this->db->commit();
+            return true;
+        } catch (Exception $e) {
+            $this->db->rollBack();
+            return false;
+        }
     }
 
     public function getAll() {
