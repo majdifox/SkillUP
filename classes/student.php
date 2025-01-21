e01c56b315fd9a9483d93e806226578f9476861b
majmehdi <majmehdi20@gmail.com>
2025-01-21 17:28:24 +0100

    getting here enrolled courses for the student

--- classes/student.php
+++ classes/student.php
@@ -1,25 +1,23 @@
 <?php
 
-require_once 'users.php';
-
-class student extends users {
-
-    public function __construct($db, $userData) {
-        parent::__construct($db);
+class Student extends Users {
+    public function __construct($db, $userData = null) {
+        parent::__construct($db, $userData);
         $this->role = 'student';
-        if ($userData) {
-            $this->id = $userData['user_id'];
-            $this->username = $userData['username'];
-            $this->email = $userData['email'];
-        }
     }
-
+// getting here enrolled courses
     public function getData() {
-
-        $query = "SELECT * FROM course JOIN enroll where enroll.student_username= :student_id";
-
+        $query = "SELECT c.*, e.enrolled_at, u.username as instructor_name, cat.name as category_name, GROUP_CONCAT(t.name) as tags FROM courses c 
+                  LEFT JOIN categories cat ON c.category_id = cat.category_id 
+                  JOIN enrollments e ON c.course_id = e.course_id 
+                  LEFT JOIN users u ON c.teacher_id = u.user_id
+                  LEFT JOIN course_tags ct ON c.course_id = ct.course_id
+                  LEFT JOIN tags t ON ct.tag_id = t.tag_id
+                  WHERE e.student_id = :student_id
+                  GROUP BY c.course_id";
+        
         $stmt = $this->db->prepare($query);
-        $stmt->bindParam(':student_id' , $this->id);
+        $stmt->bindParam(':student_id', $this->id);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
